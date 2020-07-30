482d1af96c400368aabad55dace111f193182549
Jaclyn <junwoo97@nate.com>
2020-07-31 02:01:14 +0900

    07/31 07, 08 DB 조회 및 상세페이지 작성

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,9 +1,15 @@
 const models = require('../../models');
 
 exports.get_products = ( _ , res) => {
-    res.render( 'admin/products.html' , 
-        { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
-    );
+    // res.render( 'admin/products.html' , 
+    //     { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
+    // );
+
+    // DB SELECT (조회) 실행
+    models.Products.findAll({
+    }).then( (result) => {
+        res.render('admin/products.html', {products : result}) // 템플릿 변수명과 보낼 변수명이 일치하면 products 하나만 적어도 됨.
+    });
 }
 
 exports.get_products_write = ( _ , res) => {
@@ -25,4 +31,11 @@ exports.post_products_write = ( req , res ) => {
     // models.Products.create(req.body).then( () => {
     //     res.redirect('/admin/products');
     // });
+}
+
+exports.get_products_detail = (req,res) => {
+    models.Products.findByPk(req.params.id).then( (result) => {
+        console.log(result);
+        res.render('admin/detail.html', { product : result });
+    });
 }
