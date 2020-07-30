38d0c64e4b71edf1fb9097c8cfea12cfbb85734c
Jaclyn <junwoo97@nate.com>
2020-07-31 02:09:57 +0900

    07/31 09. 날짜포매팅(moment.js) 적용

--- models/Products.js
+++ models/Products.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 // table 생성(모델 생성) 작업
 module.exports = function(sequelize, DataTypes){
     const Products = sequelize.define('Products',
@@ -8,5 +10,14 @@ module.exports = function(sequelize, DataTypes){
             description : { type: DataTypes.TEXT }
         }
     );
+    
+    //moment.js를 이용한 날짜 포매팅
+    //prototype을 이용해 Products 클래스에 함수 추가
+    Products.prototype.dateFormat = (date) => {
+        return moment(date).format('YYYY년 MM월 DD일');
+    }
+    
     return Products;
+
+
 } 
