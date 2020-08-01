653dbf829b94b4d052e5cf51b382553874f186b5
Jaclyn <junwoo97@nate.com>
2020-08-02 02:45:33 +0900

    08.02 03. async await

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,31 +1,35 @@
 const models = require('../../models');
 
-exports.get_products = ( _ , res) => {
+exports.get_products = async ( _ , res) => {
     // res.render( 'admin/products.html' , 
     //     { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
     // );
 
     // DB SELECT (조회) 실행
-    models.Products.findAll({
-    }).then( (result) => {
-        res.render('admin/products.html', {products : result}) // 템플릿 변수명과 보낼 변수명이 일치하면 products 하나만 적어도 됨.
-    });
+    //async로 await로 
+    try{
+        const products = await models.Products.findAll();
+        res.render('admin/products.html', {products}); // 템플릿 변수명과 보낼 변수명이 일치하면 products 하나만 적어도 됨.
+    }catch(e){
+
+    }
+    
+
 }
 
-exports.get_products_write = ( _ , res) => {
-    res.render( 'admin/write.html');
+exports.get_products_write = async ( _ , res) => {
+    await res.render( 'admin/write.html');
 }
 
-exports.post_products_write = ( req , res ) => {
+exports.post_products_write = async ( req , res ) => {
     //res.send(req.body);
     // Products 테이블에 Insert
-    models.Products.create({
+    await models.Products.create({
         name : req.body.name,
         price : req.body.price,
         description : req.body.description
-    }).then( () => {
-        res.redirect('/admin/products');
     });
+    res.redirect('/admin/products');
 
     // // 만약에 post data와 테이블이 매핑이 되면 아래처럼 insert문을 쓸 수도 있음
     // models.Products.create(req.body).then( () => {
@@ -34,46 +38,40 @@ exports.post_products_write = ( req , res ) => {
 }
 
 // SELECT 문
-exports.get_products_detail = (req,res) => {
-    const product1 = models.Products.findByPk(req.params.id).then( (result) => {
-        //console.log(result);
-        
-    });
+exports.get_products_detail = async (req,res) => {
+    const product = await models.Products.findByPk(req.params.id);
+    res.render('admin/detail.html', {product : product});
 
-    const product2 = models.Products.findByPk(req.params.id).then( (result) => {
-        //console.log(result);
+    // const product2 = models.Products.findByPk(req.params.id).then( (result) => {
+    //     //console.log(result);
         
-    });
-    res.render('admin/detail.html', { product1 : product1, product2 : product2 });
+    // });
+ 
 }
 
-exports.get_products_edit = (req,res) => {
-    models.Products.findByPk(req.params.id).then( (product) => {
-        res.render('admin/write.html', {product});
-    });
+exports.get_products_edit = async (req,res) => {
+    const product = await models.Products.findByPk(req.params.id)
+    res.render('admin/write.html', {product});
+    
 }
 
 //Update
-exports.post_products_edit = (req,res) => {
-    models.Products.update({
+exports.post_products_edit = async (req,res) => {
+    await models.Products.update({
         name : req.body.name,
         price : req.body.price,
         description : req.body.description //update 할 데이터
     }, {
         where : {id : req.params.id} //조건절
-    }
-    ).then(() => {
-        res.redirect('/admin/products/detail/' + req.params.id);
-    }) 
+    });
+    res.redirect('/admin/products/detail/' + req.params.id);
 }
 
 //DELETE 문
-exports.get_products_delete = (req,res) => {
-    models.Products.destroy({
+exports.get_products_delete = async (req,res) => {
+    await models.Products.destroy({
         where : {
             id : req.params.id
-        }
-    }).then(() => {
-        res.redirect('/admin/products');
-    });
+        }});
+    res.redirect('/admin/products');
 }
--- promise_ex/promise_chaning.js
+++ promise_ex/promise_chaning.js
@@ -1,13 +1,15 @@
 const p1 = new Promise((resolve,reject)=> {
     setTimeout(()=> {
+        console.log("5초 뒤 실행");
         resolve({p1_text : "p1의 텍스트"});
-    }, 1000);
+    }, 5000);
 });
 
 const p2 = new Promise((resolve,reject)=> {
     setTimeout(()=> {
+        console.log("8초 뒤 실행");
         resolve({p2_text : "p2의 텍스트"});
-    }, 3000);
+    }, 8000);
 });
 
 // 이 과정을 promise all로 할 수 있음
@@ -21,13 +23,27 @@ const p2 = new Promise((resolve,reject)=> {
 
 //Promise all
 //p1,p2가 동시에 시작되기 때문에 총 3초가 걸림
-Promise.all([p1,p2]).then((result) => {
-    console.log("p1 = " + result[0].p1_text);
-    console.log("p2 = " + result[1].p2_text);
-})
+// Promise.all([p1,p2]).then((result) => {
+//     console.log("p1 = " + result[0].p1_text);
+//     console.log("p2 = " + result[1].p2_text);
+// })
 
-//Promise race
-//가장 먼저 종료된 promise객체의 결과만 가져옴
-Promise.race([p1,p2]).then((result => {
-    console.log(result);
-}))
+// //Promise race
+// //가장 먼저 종료된 promise객체의 결과만 가져옴
+// Promise.race([p1,p2]).then((result => {
+//     console.log(result);
+// }))
+
+(async () => { //await 두개 연속 시 동시에 실행 됌
+    const a = await p1;
+  
+    console.log(a);
+    console.log("test");
+    
+})();
+
+(async () => {
+    const b = await p2;
+    console.log(b);
+    console.log("test2");
+})();
